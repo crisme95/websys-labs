618a381fb4fcda020ff925af0c8d093b9a8855b9
Cristian Mejia <80281700+crisme95@users.noreply.github.com>
2021-11-21 16:28:32 -0500

    Created script.js, updated index.php
    Created the JS file and added a header to index.php

--- lab8/index.php
+++ lab8/index.php
@@ -5,10 +5,11 @@
   <meta charset="utf-8">
   <title>Lab 8</title>
   <link rel="stylesheet" href="assets/style.css" />
-  
-
+  <script src="assets/script.js" defer></script>
 </head>
 
-<body></body>
+<body>
+  <h1>Welcome to Web Systems Development Fall 2021</h1>
+</body>
 
 </html>
